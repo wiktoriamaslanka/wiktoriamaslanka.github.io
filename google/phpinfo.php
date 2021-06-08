4029ff58f9085324482c616b94fd1a3721c83d9b
wiktoriamaslanka <77620508+wiktoriamaslanka@users.noreply.github.com>
2021-06-08 11:42:42 +0200

    Update phpinfo.php

--- google/phpinfo.php
+++ google/phpinfo.php
@@ -1,12 +1,3 @@
-{\rtf1\ansi\ansicpg1250\cocoartf1671\cocoasubrtf600
-{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red242\green242\blue242;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c96078\c96078\c96078;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww12200\viewh15740\viewkind1
-\deftab720
-\pard\pardeftab720\sl360\partightenfactor0
-
-\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 <?php\
-phpinfo();\
-?>}
+<?php
+phpinfo();
+?>
